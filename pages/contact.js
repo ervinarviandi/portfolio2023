9ae62c05967b16c0a0248db5307b3d198711a0e2
Ervin Arviandi <muhamadervin34@gmail.com>
2023-01-25 19:23:53 -0800

    tambah google maps api

--- pages/contact.js
+++ pages/contact.js
@@ -1,26 +1,66 @@
 import React from "react";
 import Link from "next/link";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
 
 function errorPage() {
   return (
     <>
-      <div className="px-4 py-12">
-        <div className="mt-[70px] mx-auto md:max-w-3xl xl:max-w-3xl relative">
-          <div className="flex justify-center text-center items-center">
-            {/* <img src="/undraw_page_not_found_re_e9o6.svg" width={300} /> */}
-            <img src="/404 error with person looking for-bro.svg" className="md:w-[400px] w-[200px]" />
-          </div>
-          <h2 className="justify-center items-center text-center font-bold text-3xl font-poppins text-pink-600">Ooopss Page Not Found</h2>
-          <p className="dark:text-white text-gray-700 justify-center text-center text-base font-poppins mt-5">
-            We are sorry but the page you are looking for does not exist.
-            <br /> click{" "}
-            <Link href="/" className="text-pink-500 font-bold  ">
-              Here
-            </Link>{" "}
-            for go back home
-          </p>
-        </div>
-      </div>
+      <Navigation />
+      <main className="px-4 py-12">
+        <section className="mt-[96px] mx-auto md:max-w-4xl  xl:max-w-4xl ">
+          <h1 className=" font-poppins text-3xl dark:text-white text-gray-700 font-bold">Contact</h1>
+          <main className="md:flex lg:flex dark:bg-black mt-10 ">
+            {/* Headline */}
+            <div className="md:w-7/12">
+              <form>
+                <div className="w-full lg:w-7/12 ">
+                  <div className="w-full px-4 mb-8">
+                    <label for="name" class="text-base font-bold text-primary">
+                      Nama
+                    </label>
+                    <input type="text" id="name" className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-pink-500 focus:ring-1 focus:border-pink-500" />
+                  </div>
+                  <div className="w-full px-4 mb-8">
+                    <label for="email" class="text-base font-bold text-primary">
+                      Email
+                    </label>
+                    <input type="email" id="email" className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-pink-500 focus:ring-1 focus:border-pink-500" />
+                  </div>
+                  <div class="w-full px-4 mb-8">
+                    <label for="message" className="text-base font-bold text-primary">
+                      Pesan
+                    </label>
+                    <input type="message" id="email" className="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-pink-500 focus:ring-1 focus:border-pink-500 h-32" />
+                  </div>
+                  <div className="relative w-full px-4">
+                    <button className=" absolute flex font-poppins text-base items-center  border-2 dark:text-white text-gray-700 bg-transparent py-2 px-5 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-pink-500 before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 rounded-lg ">
+                      Kirim
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            {/* Hero */}
+            <div className=" bg-purple-300 h-96 w-full md:w-5/12  mt-20">
+              <figure>
+                <iframe
+                  className="w-full h-96 "
+                  width=""
+                  id="gmap_canvas"
+                  src="https://maps.google.com/maps?q=wawo,Dompu,%20West%20Nusa%20Tenggara&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                  frameborder="0"
+                  scrolling="no"
+                  marginheight="0"
+                  marginwidth="0"
+                ></iframe>
+              </figure>
+            </div>
+          </main>
+        </section>
+      </main>
+      <Footer />
     </>
   );
 }
