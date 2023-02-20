4f65f5a364e67fd41d0e05cfb46ea616b4ca4dfe
Ervin Arviandi <muhamadervin34@gmail.com>
2023-02-20 02:48:33 -0800

    update pages questions

--- pages/Questions.js
+++ pages/Questions.js
@@ -2,13 +2,15 @@ import React from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import Questions from "../components/Questions";
+import Image from "next/image";
+import Faq from "../public/undraw_questions_re_1fy7.svg";
 
 function Question() {
   return (
     <>
       <Navigation />
       <div className="px-4 py-12">
-        <div className="mt-[96px] mx-auto md:max-w-4xl  xl:max-w-4xl px-4">
+        <div className="mt-[96px] mx-auto md:max-w-4xl  xl:max-w-4xl px-4 ">
           <div className="">
             <div className="mb-10">
               <h2
@@ -17,6 +19,9 @@ function Question() {
               >
                 Questions
               </h2>
+              <div className="mt-20">
+                <Image src={Faq} alt="Questions" width="400" />
+              </div>
             </div>
             <p className="dark:text-white text-gray-700 font-poppins text-base mt-4">Di Bawah Ini adalah Beberapa pertanyaan Yang Sering Di tanyakan</p>
             <Questions />
