aba0c6a6d2f45724782910b530d5b3d0d069f542
Ervin Arviandi <muhamadervin34@gmail.com>
2023-04-25 02:52:29 -0700

    menambahkan attention pada hero

--- components/Questions.js
+++ components/Questions.js
@@ -10,7 +10,7 @@ function Questions() {
             <Disclosure>
               {({ open }) => (
                 <>
-                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                  <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4  py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                     <span className="font-poppins">Teknologi apa aja sih yang lu gunakan untuk mengembangkan website?</span>
                     <ChevronUpIcon className={`${open ? "rotate-180 transform" : ""} h-5 w-5 text-purple-500`} />
                   </Disclosure.Button>
